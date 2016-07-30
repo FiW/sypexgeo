7f5b0f45a693b66150bc2d4fc9cf15ab3fabfdd7
Alexander Kuznetsov <alexander@kuznetsov.online>
2016-07-30 21:30:41 +0300

    small fixes

--- sypexgeo.js
+++ sypexgeo.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var app = new express();
-var http = require('http');
 var sypex = require('sypexgeo-vyvid');
 var sypexgeo = new sypex.Geo('SxGeoCity.dat');
 
@@ -9,6 +8,4 @@ app.use(function(req, res) {
         res.json(sypexgeo.find(req.url.substr(1)));
 });
 
-http
-.createServer(app)
-.listen(3000);
+app.listen(3000);
